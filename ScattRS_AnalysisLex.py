621842f4ebf89d3ec124fe1503bf1d0f1588c474
Mickey1412 <marochac96@gmail.com>
2019-03-16 17:15:44 -0600

    Corrections Lex and Parser
    Made corrections for both files

--- ScattRS_AnalysisLex.py
+++ ScattRS_AnalysisLex.py
@@ -3,10 +3,36 @@
 #11/03/2019
 
 import ply.lex as lex
+import sys
 
 #Lista de Tokens del lenguaje
-Tokens = ['OP_SUMA', 'OP_RESTA', 'OP_MULT', 'OP_DIV', 'PAREN_I', 'PAREN_D', 'SEMICOLON', 'COMA', 'CURLY_I', 'CURLY_D', 'BRACKET_I', 'BRACKET_D', 'EQUAL', 'MENOR_EQ', 'MAYOR_EQ', 'MENOR', 'MAYOR', 'NOT_EQ',
- 			'ASSIGN', 'ID', 'INT_VALOR', 'FLOAT_VALOR', 'BOOL_VALOR', 'CHAR_VALOR', 'STRING_VALOR']
+tokens = [
+	'OP_SUMA', 
+	'OP_RESTA', 
+	'OP_MULT', 
+	'OP_DIV', 
+	'PAREN_I', 
+	'PAREN_D', 
+	'SEMICOLON', 
+	'COMA', 
+	'CURLY_I', 
+	'CURLY_D', 
+	'BRACKET_I', 
+	'BRACKET_D', 
+	'EQUAL', 
+	'MENOR_EQ', 
+	'MAYOR_EQ', 
+	'MENOR', 
+	'MAYOR', 
+	'NOT_EQ',
+	'ASSIGN', 
+	'ID', 
+	'INT_VALOR', 
+	'FLOAT_VALOR', 
+	'CHAR_VALOR', 
+	'STRING_VALOR',
+	'COLON'
+	]
 
 #Palabras reservadas = 'lexema' : 'token'
 p_reservadas = {
@@ -44,7 +70,7 @@ p_reservadas = {
 }
 
 #Union de los tokens de las palabras reservadas y tokens de operadores
-Tokens = Tokens + list(p_reservadas.values())
+tokens = tokens + list(p_reservadas.values())
 
 #Expresiones regulares de los tokens
 t_OP_SUMA = r'\+'
@@ -57,8 +83,8 @@ t_SEMICOLON = r';'
 t_COMA = r','
 t_CURLY_I = r'\{'
 t_CURLY_D = r'\}'
-t_BRAKET_I = r'\['
-t_BRAKET_D = r'\]'
+t_BRACKET_I = r'\['
+t_BRACKET_D = r'\]'
 t_EQUAL = r'=='
 t_COLON = r':'
 t_MENOR = r'<'
@@ -67,32 +93,31 @@ t_MENOR_EQ = r'<='
 t_MAYOR_EQ = r'>='
 t_NOT_EQ = r'!='
 t_ASSIGN = r'='
+t_CHAR_VALOR = r'\'.?\''
+t_STRING_VALOR = r'\".*\"'
+#Token para ignorar
+t_ignore = ' \t'
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     t.type = p_reservadas.get(t.value, 'ID')
     return t
 
+def t_FLOAT_VALOR(t):
+    r'-?[0-9]+\.[0-9][0-9]*'
+    return t
+	
 def t_INT_VALOR(t):
     r'-?[0-9]+'
     return t
 
-def t_FLOAT_VALOR(t):
-    r'-?[0-9]+\.[0-9][0-9]*'
-    return t
-
-t_BOOL_VALOR = r'((true|TRUE)|(false|FALSE))'
-t_CHAR_VALOR = r'\‘.* \’'
-t_STRING_VALOR = r'\".*\" | \'.*\''
-
-#Token para ignorar
-t_ignore = ' \t'
+def t_error(t):
+    print("Illegal characters!")
+    t.lexer.skip(1)
 
 #Correr el analizador lexico
 ScattRS_AnalysisLex = lex.lex()
-input_data = ''
-ScattRS_AnalysisLex.input(input_data)
-
+ScattRS_AnalysisLex.input("\"Hola\"")
 while True:
     token = ScattRS_AnalysisLex.token()
     if not token:
